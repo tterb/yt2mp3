1fe652f9df616757c422fb254bda643eb440cf32
JonSn0w <bstevensondev@gmail.com>
2018-03-25 17:37:26 -0700

    :space_invader: Add MP3 metadata manipulation functionality

--- yt-to-mp3.py
+++ yt-to-mp3.py
@@ -1,27 +1,66 @@
 from pydub import AudioSegment
-from time import sleep
-import sys, os, pafy
+from mutagen.easyid3 import EasyID3
+import sys, os, pafy, argparse
 
 def main():
-  url = sys.argv[1]
-  video = pafy.new(url)
+  parser = argparse.ArgumentParser(description='YouTube to MP3 Converter')
+  parser.add_argument('-i','--info', action="store_true", help='Add ID3 tags to the generated MP3 file')
+  parser.add_argument('url', help='The YouTube URL you want to convert')
+  args = parser.parse_args()
+  video = pafy.new(args.url)
   ext = getAudio(video)
   # Convert to MP3
   convertMP3(video.title, ext)
   # Cleanup
   os.remove(video.title+'.'+ext)
-  os.rename(video.title+'.mp3', os.environ['HOME']+'/Downloads/'+video.title+'.mp3')
+  if args.info:
+    d = getInfo(video.title)
+    setID3(d)
+    os.rename(video.title+'.mp3', os.environ['HOME']+'/Downloads/'+d['title']+' - '+d['artist']+'.mp3')
+  else:
+    os.rename(video.title+'.mp3', os.environ['HOME']+'/Downloads/'+video.title+'.mp3')
 
-
+# Get best audio stream from video
 def getAudio(video):
   print(f' Title: {video.title}\n Length: {video.duration}')
   best = video.getbestaudio()
   best.download(quiet=True, callback=printProgressBar)
   return best.extension
 
+# Convert audio file to MP3
 def convertMP3(title, ext):
   audio = AudioSegment.from_file(title+'.'+ext, format=ext)
   audio.export(title+'.mp3', format='mp3')
+
+# Get audio info for ID3 tags
+def getInfo(vid_title):
+  d = {'video' : vid_title}
+  info = []
+  if ' - ' in vid_title:
+    info = vid_title.split(' - ')
+  else:
+    info = [vid_title]
+  info = [i.strip('\'"').replace('_', ' ').title() for i in info]
+  for n in ['Title', 'Artist']:
+    for j,i in enumerate(info):
+      inp = input(f'{n}: {i}? (y/n) ')
+      if inp.lower() in ['y', 'yes']:
+        d[n.lower()] = i
+        break
+      elif j == len(info)-1:
+        d[n.lower()] = input(f'{n}: ')
+  d['album'] = input('Album: ')
+  d['genre'] = input('Genre: ')
+  return d
+
+# Set MP3 ID3 tags
+def setID3(d):
+  song = EasyID3(d['video']+'.mp3')
+  song['title'] = d['title']
+  song['artist'] = d['artist']
+  song['album'] = d['album']
+  song['genre'] = d['genre']
+  song.save()
 
 # Print iterations progress
 def printProgressBar(size, recvd, ratio, rate, eta, prefix = '', suffix = '', total = 100, decimals = 1, length = 45, fill = '█'):
@@ -34,6 +73,7 @@ def printProgressBar(size, recvd, ratio, rate, eta, prefix = '', suffix = '', to
   if iteration == total: 
     print()
     print(u' \u2713 Complete')
+
 
 if __name__ == '__main__':
   main()
