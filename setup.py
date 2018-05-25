516b304cf20a6f3a58c8f1063d58edb4243b2a7c
tterb <bstevensondev@gmail.com>
2018-05-25 00:19:37 -0700

    :package: Modify PyPi configuration file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Brett Stevenson',
   author_email = 'bstevensondev@gmail.com',
   url = 'https://github.com/tterb/yt2mp3',
-  download_url = 'https://github.com/tterb/yt2mp3/archive/0.1.tar.gz',
+  download_url = 'https://github.com/tterb/yt2mp3/archive/1.0.tar.gz',
   keywords = ['youtube', 'mp3', 'download', 'itunes', 'music'],
   classifiers = [],
 )
