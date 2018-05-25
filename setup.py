3fe6a2108bb0b9362b12b96f342d895e63e2d60a
tterb <bstevensondev@gmail.com>
2018-05-25 02:05:27 -0700

    :package: Modify PyPi configuration

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
   name = 'yt2mp3',
-  packages = ['yt2mp3'],
-  version = '1.0',
+  version = '1.0.2',
   description = 'Simplifies the process of downloading and converting Youtube videos to MP3 files',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Brett Stevenson',
   author_email = 'bstevensondev@gmail.com',
   url = 'https://github.com/tterb/yt2mp3',
-  download_url = 'https://github.com/tterb/yt2mp3/archive/1.0.tar.gz',
+  download_url = 'https://github.com/tterb/yt2mp3/archive/1.0.2.tar.gz',
   keywords = ['youtube', 'mp3', 'download', 'itunes', 'music'],
-  classifiers = [],
+  packages = setuptools.find_packages(),
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python :: 3.4', 'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Natural Language :: English'
+  ],
 )
