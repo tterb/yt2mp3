6cacd3f6a4e041f3f1fba8863ced66b945c18e63
tterb <bstevensondev@gmail.com>
2018-06-30 23:23:32 -0700

    :package: Fix issue #7: missing package executable

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ with open('README.md') as f:
 
 setuptools.setup(
   name = 'yt2mp3',
-  version = '1.0.2',
-  description = 'Simplifies the process of downloading and converting Youtube videos to MP3 files',
+  version = '1.0.3',
+  description = 'Simplifies the process of searching, downloading and converting Youtube videos to MP3 files',
   long_description=long_description,
   long_description_content_type="text/markdown",
   author = 'Brett Stevenson',
   author_email = 'bstevensondev@gmail.com',
   url = 'https://github.com/tterb/yt2mp3',
-  download_url = 'https://github.com/tterb/yt2mp3/archive/1.0.2.tar.gz',
-  keywords = ['youtube', 'mp3', 'download', 'itunes', 'music'],
+  download_url = 'https://github.com/tterb/yt2mp3/archive/1.0.3.tar.gz',
+  keywords = ['youtube', 'convert', 'mp3', 'download', 'itunes', 'music', 'cli'],
   packages = setuptools.find_packages(),
+  scripts=['bin/yt2mp3'],
   classifiers = [
     'Development Status :: 4 - Beta',
     'Programming Language :: Python :: 3.4', 'Programming Language :: Python :: 3.5',
