00547f03d19cea61bf665964678326f828e5200e
tterb <bstevensondev@gmail.com>
2018-07-07 02:47:49 -0700

    :rocket: Improve playlist support functionality

--- yt2mp3/__init__.py
+++ yt2mp3/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
 yt2mp3
-A program that simplifies the process of searching, downloading and converting
-Youtube videos to MP3 files with embedded metadata via the iTunes API.
+A program that simplifies the process of searching, downloading and converting Youtube videos to MP3 files with embedded metadata via the iTunes API.
 yt2mp3/__init__.py
 Brett Stevenson (c) 2018
 """
@@ -33,7 +32,7 @@ def getSongData(track, artist, exit=True):
         for s in album.get_tracks():
           songs.append(s)
       return songs
-    # Attempt to find a close match if no exact matches found
+    # Attempt to find a close match if no exact matches
     song = itunespy.search(' '.join([track,artist]))[0]
     if song:
       return song
@@ -42,34 +41,25 @@ def getSongData(track, artist, exit=True):
       logging.warning(str(e))
       sys.exit()
 
-# Attempt to retrieve song data from URL
-def getVideoData(url):
-  # Get YouTube video title
-  req = Request(url, headers={'User-Agent':'Mozilla/5.0'})
-  response = urlopen(req, context=ssl._create_unverified_context())
-  soup = BeautifulSoup(response.read(), 'lxml')
-  title = soup.find('span', { 'class':'watch-title' }).get_text().strip()
-  # Remove parenthesis contents and commonly added words
-  title = re.sub(re.compile(r'\([^)]*\)|[[^]]*\]|ft|feat|\blyrics?\b|official|video|audio', re.IGNORECASE), '', title)
-  title = title.translate(str.maketrans('','',string.punctuation))
-  title = ' '.join(title.split())
+# Attempt to retrieve song data from the video title
+def getVideoData(title):
+  # Remove parenthesis, punctuation and commonly added words
+  keywords = re.sub(re.compile(r'\([^)]*\)|[[^]]*\]|ft|feat|\blyrics?\b|official|video|audio', re.IGNORECASE), '', title)
+  keywords = keywords.translate(str.maketrans('','',string.punctuation))
+  keywords = ' '.join(keywords.split())
   # Query iTunes API
   try:
-    return itunespy.search(title)[0]
+    return itunespy.search(keywords)[0]
   except LookupError:
     pass
-    
+
+# Get YouTube video title
 def getVideoTitle(url):
   # Get YouTube video title
   req = Request(url, headers={'User-Agent':'Mozilla/5.0'})
   response = urlopen(req, context=ssl._create_unverified_context())
   soup = BeautifulSoup(response.read(), 'lxml')
-  title = soup.find('span', { 'class':'watch-title' }).get_text().strip()
-  # Remove parenthesis contents and commonly added words
-  keywords = re.sub(re.compile(r'\([^)]*\)|[[^]]*\]|ft|feat|\blyrics?\b|official|video|audio', re.IGNORECASE), '', title)
-  keywords = keywords.translate(str.maketrans('','',string.punctuation))
-  keywords = ' '.join(keywords.split())
-  return title, keywords
+  return soup.find('span', { 'class':'watch-title' }).get_text().strip()
 
 # Displays an interactive menu of songs
 def showMenu(options):
@@ -79,7 +69,7 @@ def showMenu(options):
     sys.exit()
   return selection
   
-# Scrapes youtube for a video that has track and artist in the name
+# Scrapes YouTube for a video with the track and artist
 def getURL(track, artist):
   query = urllib.parse.quote(track+" "+artist)
   url = 'https://www.youtube.com/results?search_query=' + query
@@ -90,6 +80,31 @@ def getURL(track, artist):
   for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
     results.append('https://www.youtube.com' + vid['href'])
   return results[0]
+
+# Returns a list of video URLs in playlist
+def getVideoList(url):
+  yt = pytube.Playlist(url)
+  video_list = ['https://www.youtube.com'+i for i in yt.parse_links()]
+  return video_list
+
+# Downloads each of the songs from the playlist
+def downloadPlaylist(videos):
+  logging.info('Downloading...')
+  for i, url in enumerate(videos):
+    title = getVideoTitle(url)
+    logging.info(str(i+1)+' of '+str(len(videos))+': '+str(title))
+    result = getVideoData(title)
+    if not result:
+      track = input(' Track: ')
+      artist = input(' Artist: ')
+      result = getSongData(track, artist)
+    if result:
+      song = Song(defaultdict(str, result.__dict__))
+      song.video_url = url
+    tempPath = download(url)
+    path = convertToMP3(tempPath, song)
+    setData(path, song)
+  logging.info(' ✔ Done')
 
 # Downloads the video at the provided url 
 def download(url, progressBar=False):
@@ -104,31 +119,6 @@ def download(url, progressBar=False):
   yt.streams.filter(subtype='mp4', progressive=True).first().download(tempDir, id)
   logging.info(' ✔ Download Complete')
   return glob.glob(os.path.join(str(tempDir), id+'.*'))[0]
-
-# Downloads each of the songs from the playlist
-def downloadPlaylist(videos):
-  for i, url in enumerate(videos):
-    title, keywords = getVideoTitle(url)
-    logging.info(str(i+1)+' of '+str(len(videos))+': '+str(title))
-    result = getVideoData(url)
-    if not result:
-      print(title)
-      track = input(' Track: ')
-      artist = input(' Artist: ')
-      result = getSongData(track, artist)
-    if result:
-      song = Song(defaultdict(str, result.__dict__))
-      song.video_url = url
-    tempPath = download(url)
-    path = convertToMP3(tempPath, song)
-    setData(path, song)
-  logging.info(' ✔ Done')
-
-# Returns a list of video URLs in playlist
-def getVideoList(url):
-  yt = pytube.Playlist(url)
-  video_list = ['https://www.youtube.com'+i for i in yt.parse_links()]
-  return video_list
 
 # Convert the downloaded video file to MP3
 def convertToMP3(tempPath, song):
