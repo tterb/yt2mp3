1a18e57aeb8bdf5985d9290bd01e9cfef7e75c82
tterb <bstevensondev@gmail.com>
2018-08-07 07:58:48 -0700

    :bookmark: Improve command-line option parsing

--- yt2mp3/opts.py
+++ yt2mp3/opts.py
@@ -1,14 +1,14 @@
 import argparse
 
-def getOptions():
+def parseOptions(args):
   parser = argparse.ArgumentParser(prog='yt2mp3', usage='%(prog)s [options]', add_help=True)
-  parser.add_argument('--version', action='version', version='v1.0.5', help='show the program version number and exit')
-  parser.add_argument('-t', '--track', default='', help='Specify the track name query', nargs='+')
-  parser.add_argument('-a', '--artist', default='', help='specify the artist name query', nargs='+')
+  parser.add_argument('--version', action='version', version='v1.1.0', help='show the program version number and exit')
+  parser.add_argument('-t', '--track', nargs='+', help='Specify the track name query', default='')
+  parser.add_argument('-a', '--artist', nargs='+', help='specify the artist name query', default='')
   parser.add_argument('-u', '--url', help='specify the YouTube URL/ID of the video to convert')
   parser.add_argument('-p', '--playlist', help='specify the YouTube URL/ID of the playlist to convert')
-  parser.add_argument('-r', '--res', help='specify the resolution for the cover-art image')
-  parser.add_argument('-o', '--overwrite', help='overwrite file if one exists in output directory', action='store_true')
-  parser.add_argument('-v', '--verbose', help='display a download progress bar', action='store_true')
-  parser.add_argument('-q', '--quiet', help='suppress command-line output', action='store_true')
-  return parser.parse_args()
+  parser.add_argument('-r', '--resolution', type=int, help='specify the resolution for the cover-art image', default=480)
+  parser.add_argument('-o', '--overwrite', action='store_true', help='overwrite file if one exists in output directory')
+  parser.add_argument('-v', '--verbose', action='store_true', help='display a download progress bar')
+  parser.add_argument('-q', '--quiet', action='store_true', help='suppress command-line output')
+  return parser.parse_args(args)
