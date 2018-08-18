f32b13c28cf2aa6558c3797d4e91e856c7f3c71b
tterb <bstevensondev@gmail.com>
2018-08-17 18:58:27 -0700

    :bookmark: Update program command-line options

--- yt2mp3/opts.py
+++ yt2mp3/opts.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python3
+"""
+yt2mp3
+A program that simplifies the process of searching, downloading and 
+converting Youtube videos to MP3 files with embedded metadata via the 
+iTunes API.
+yt2mp3/opts.py
+Brett Stevenson (c) 2018
+"""
+
 import argparse
 
 def parseOptions(args):
   parser = argparse.ArgumentParser(prog='yt2mp3', usage='%(prog)s [options]', add_help=True)
-  parser.add_argument('--version', action='version', version='v1.1.0', help='show the program version number and exit')
-  parser.add_argument('-t', '--track', nargs='+', help='Specify the track name query', default='')
+  parser.add_argument('--version', action='version', version='v1.2.0', help='show the program version number and exit')
+  parser.add_argument('-t', '--track', nargs='+', help='specify the track name query', default='')
   parser.add_argument('-a', '--artist', nargs='+', help='specify the artist name query', default='')
   parser.add_argument('-u', '--url', help='specify the YouTube URL/ID of the video to convert')
   parser.add_argument('-p', '--playlist', help='specify the YouTube URL/ID of the playlist to convert')
