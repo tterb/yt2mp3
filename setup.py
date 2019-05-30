8fd7184de922bec30abbe4f1dc9423798e78dee7
Brett Stevenson <therealbrettstevenson@gmail.com>
2019-05-30 16:19:40 -0700

    :arrow_up: Update package dependency versions

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     'beautifulsoup4>=4.6.3',
     'colorama==0.4.1',
     'curses_menu>=0.5.0',
-    'importlib_metadata>=0.12',
+    'importlib_metadata>=0.9',
     'itunespy==1.5.5',
     'lxml>=4.2.5',
     'mutagen==1.42.0',
